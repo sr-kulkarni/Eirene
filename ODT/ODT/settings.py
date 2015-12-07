25564bf79e1139d3fa82095c5801fb1e1e14c1c1
Saurabh Kulkarni <Saurabh@Saurabhs-MacBook-Pro-3.local>
2015-12-06 16:54:49 -0800

    C6 Official Django Tutorial - Level 2

--- ODT/ODT/settings.py
+++ ODT/ODT/settings.py
@@ -14,7 +14,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+T_DIR = os.path.join(BASE_DIR,'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ODT.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [T_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

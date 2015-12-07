6b37e447ec1019dd194bb3811a306e7684ae0849
Saurabh Kulkarni <Saurabh@Saurabhs-MacBook-Pro-3.local>
2015-12-06 19:40:33 -0800

    C8 - Added Category views/URL Patterns and Regex

--- site1/app1/views.py
+++ site1/app1/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from app1.models import Category
+from app1.models import Category,Page 
 
 
 # Create your views here.
@@ -14,3 +14,17 @@ def about(request):
 	context_dict = {'boldmessage': "Supremely Awesome!!"}
 	return render(request,'app1/about.html',context_dict)
 	#return HttpResponse("This is the about page. Do you wanna go back to the <a href='/app1/'>main page?</a>")
+
+def category(request,category_name_slug):
+	context_dict={}
+	print category_name_slug
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+		context_dict['category_name'] = category.name 
+		pages = Page.objects.filter(category=category)
+		context_dict['pages'] = pages
+		context_dict['category'] = category
+	except Category.DoesNotExist:
+		pass
+
+	return render(request,'app1/category.html',context_dict)
--- site1/site1/urls.py
+++ site1/site1/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url, patterns
 from django.contrib import admin
 from django.conf import settings
 
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^app1/', include('app1.urls')),

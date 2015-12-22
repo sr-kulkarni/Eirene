ba15302629b7b406c57f4e0da6de35bbd645ee91
root <sr.kulkarni@icloud.com>
2015-12-22 12:32:40 -0800

    Rest Tutorial - C2

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -2,6 +2,7 @@
 from django.conf.urls import patterns, url
 from snippets import views
 
-urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'),)
-
+urlpatterns = [
+    url(r'^snippets/$', views.snippet_list),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+]

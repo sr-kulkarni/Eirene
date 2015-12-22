a2ca41e65a566faa2bf9d6d5fc8d7737a0b0771c
root <sr.kulkarni@icloud.com>
2015-12-22 15:13:58 -0800

    Added Auth mechanism

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -4,9 +4,13 @@ from django.http import HttpResponse
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import generics
-
+from snippets.serializers import UserSerializer
+from django.contrib.auth.models import User
 # Create your views here.
 #@api_view(['GET', 'POST'])
+
+from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
 
 def index(request):
         return HttpResponse("Snippets says hey there world!")
@@ -17,9 +21,22 @@ def index(request):
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     serializer_class = SnippetSerializer
-
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly,)
     serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
